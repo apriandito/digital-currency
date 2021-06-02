3a61c97b594002d971d2bed699b2d0341e6b8631
Muhammad Apriandito A.S <m.apriandito@gmail.com>
2021-06-02 23:06:42 +0700

    Minimize Sample Data

--- src/convert-data.R
+++ src/convert-data.R
@@ -23,14 +23,14 @@ df_network <- df%>%
 
 # Select Text Data - Sample
 df_text_sample <- df %>%
-  filter(created_at > dmy("20-12-2020")) %>%
+  filter(created_at > dmy("25-12-2020")) %>%
   select(created_at, text) %>%
   write_csv("data/tweet_digitalcurrency_text-sample.csv")
 
 
 # Select Network Data
 df_network_sample <- df%>%
-  filter(created_at > dmy("01-12-2020")) %>%
+  filter(created_at > dmy("25-12-2020")) %>%
   mutate(created_at = as_date(created_at)) %>%
   select(created_at, screen_name, mentions_screen_name) %>%
   unnest(mentions_screen_name) %>%
